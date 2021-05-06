cfeb29b822081473206488817a91bb36708b39c2
levimartines <levi.mferreira@gmail.com>
2021-05-06 09:55:26 -0300

    Lambda vs Interface

--- src/io/levimartines/Greeter.java
+++ src/io/levimartines/Greeter.java
@@ -10,7 +10,23 @@ public class Greeter {
 
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-        greeter.greet(new HelloWorldGreetingImpl());
+
+        // Cria uma nova instância da classe HelloWorldGreetingImpl, que implementa o método perform
+        Greeting helloWorldGreeting = new HelloWorldGreetingImpl();
+
+        // Criando uma classe anônima interna, mas ainda é diferente de uma função Lambda
+        Greeting innerClassGreeting = new Greeting() {
+            public void perform() {
+                System.out.println("Hello world from Inner Class");
+            }
+        };
+
+        // Não instanciamos um novo objeto, apenas criamos a função usando Lambda
+        Greeting lambdaGreeting = () -> System.out.println("Hello world using Lambda!");
+
+        greeter.greet(helloWorldGreeting);
+        greeter.greet(innerClassGreeting);
+        greeter.greet(lambdaGreeting);
     }
 
 }
