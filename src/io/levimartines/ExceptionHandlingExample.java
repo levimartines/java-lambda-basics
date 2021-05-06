81ac26f7f97ef5639d6ef79ff80f307307b3f1e3
Levi Martines <levi.mferreira@gmail.com>
2021-05-06 20:36:51 -0300

    Tratando exceções em funções Lambda

--- src/io/levimartines/ExceptionHandlingExample.java
+++ src/io/levimartines/ExceptionHandlingExample.java
@@ -1,5 +1,32 @@
 package io.levimartines;
 
+import java.util.function.BiConsumer;
+
 public class ExceptionHandlingExample {
+
+    public static void main(String[] args) {
+
+        int[] values = {1, 2, 3, 4};
+        int key = 0;
+
+        process(values, key, wrapperLambda((v, k) -> System.out.println(v / k)));
+    }
+
+    private static void process(int[] values, int key, BiConsumer<Integer, Integer> consumer) {
+        for (int value : values) {
+            consumer.accept(value, key);
+        }
+    }
+
+    private static BiConsumer<Integer, Integer> wrapperLambda(
+        BiConsumer<Integer, Integer> consumer) {
+        return (v, k) -> {
+            try {
+                consumer.accept(v, k);
+            } catch (ArithmeticException e) {
+                System.out.println("Exception caught in wrapper lambda");
+            }
+        };
+    }
 
 }
