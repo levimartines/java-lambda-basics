f5ca5e3e25b17b047f3278b9ee39dfd93955c054
Levi Martines <levi.mferreira@gmail.com>
2021-05-06 21:32:46 -0300

    Referência this nas expressões Lambda

---
+++ src/io/levimartines/ThisReferenceExample.java
@@ -0,0 +1,28 @@
+package io.levimartines;
+
+public class ThisReferenceExample {
+
+    public static void main(String[] args) {
+        ThisReferenceExample example = new ThisReferenceExample();
+        example.doProcess(10, i -> {
+            System.out.println("Value of i: " + i);
+            // Em expressões lambda, o this referencia o contexto de onde ela foi criada.
+            // System.out.println("This: " + this);
+        });
+
+        // Nesse caso o this do método execute referencia ao objeto example.
+        example.execute();
+
+    }
+
+    public void doProcess(int i, Process process) {
+        process.process(i);
+    }
+
+    public void execute() {
+        doProcess(10, i -> {
+            System.out.println("Value of i: " + i);
+            System.out.println("This: " + this);
+        });
+    }
+}
