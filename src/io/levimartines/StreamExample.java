ccb9cc6d414776c635733a77bb67e03a0d132b4d
levimartines <levi.mferreira@gmail.com>
2021-05-07 13:11:48 -0300

    Introdução a Stream

---
+++ src/io/levimartines/StreamExample.java
@@ -0,0 +1,32 @@
+package io.levimartines;
+
+import io.levimartines.exercises.Person;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+        List<Person> personList = Arrays.asList(
+            new Person("Chalie", "Brown", 33),
+            new Person("Marie", "Currie", 99),
+            new Person("Magnus", "Carlsen", 27),
+            new Person("Chico", "Xavier", 66),
+            new Person("Seu", "Zé", 9999),
+            new Person("Ronald", "Mc'Donald", 48)
+        );
+
+        List<String> personLastNameStartsWithC = personList.stream()
+            .filter(p -> p.getLastName().startsWith("C"))
+            .map(Person::getLastName)
+            .map(string -> string = "Last name: " + string)
+            .collect(Collectors.toList());
+
+        personLastNameStartsWithC.forEach(System.out::println);
+
+    }
+
+
+}
+
